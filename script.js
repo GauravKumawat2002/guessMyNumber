68ad272ed9fbf097c35c3c6b02b1d193eaac0c33
Gaurav Kumawat <gauravkumawat830@gmail.com>
2023-12-09 22:22:17 +0530

    Tasks Remain--> HighScore Updater, Reset Game button Logic

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const userInput = document.getElementById("userInput");
 let randomNumber = 0; // Storing random numbers
 const defaultChances = document.getElementsByClassName("defaultChances");
 const defaultHighScore = document.getElementsByClassName("highscore");
+const message = document.getElementsByClassName("message");
+
 // Uitlity Functions
 // Function to convert any string to number (Useful when I have to convert any HTML string into number)
 const stringToNumber = (string) => {
@@ -43,7 +45,6 @@ userInput.addEventListener("input", () => {
 // Function for storing number of times Check button is clicked to update Chances left
 const buttonClickRecord = () => {
   buttonClickCounter--;
-  logToConsole(buttonClickCounter);
   return buttonClickCounter;
 };
 // Resets the Chances left back to default value whenever it is invoked
@@ -63,7 +64,13 @@ const updateChancesLeft = (clickCounter) => {
 // Function for checking equality between userInput & computer gemerated number
 const checkInput = () => {
   if (stringToNumber(userInput.value) === randomNumber) {
-    console.log("You Win");
+    message[0].innerHTML = "You Win!!!";
+    checkButton[0].disabled = true;
+  } else {
+    if (stringToNumber(userInput.value) > randomNumber)
+      message[0].innerHTML = "Too High!!!!";
+    else if (stringToNumber(userInput.value) < randomNumber)
+      message[0].innerHTML = "Too Low!!";
   }
 };
 
@@ -71,6 +78,7 @@ const checkInput = () => {
 const gameResetter = (para1, para2) => {
   userInput.value = "";
   buttonClickCounter = 20;
+  message[0].innerHTML = "Start guessing...";
   // calling defaultChancesResetter() here as,
   // gameResetter() is the parent function responsible for bringing the game back to default values
   defaultChancesResetter(para1);
